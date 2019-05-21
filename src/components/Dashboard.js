95ab0396368f68b09892cd805431ed6a47e0feef
Ashwani Yadav <ashyadavash@gmail.com>
2019-05-21 13:46:30 +0530

    updates

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,16 +5,106 @@ import * as actions from '../actions';
 
 class Dashboard extends Component {
   state={
-    user: this.props.user
+    email: this.props.user.email,
+    instituteId: this.props.user.instituteId,
+    name: this.props.user.name,
+    password: ""
   }
+
+  onChange=(e)=>{
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleSubmit=(e)=>{
+    e.preventDefault();
+    
+  }
+
   render() {
     console.log(this.state.user);
     return (
-      <div className="container">
-        <p>Name: {this.state.user.name}</p>
-        <p>InstituteId: {this.state.user.instituteId}</p>
-      </div>
-    );
+      this.props.isAuthenticated ? (
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <p>Name: {this.props.user.name}</p>
+              <p>Email: {this.props.user.name}</p>
+              <p>InstituteId: {this.props.user.instituteId}</p>
+              <p>Batch: {this.props.user.batch}</p>
+              <p>Branch: {this.props.user.branch}</p>
+            </div>
+          </div>
+          <h3 className="text-center">Update Profile</h3>
+          <div className="row d-flex justify-content-center">
+            <div className="col-md-6">
+              <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input 
+                    name="email"
+                    id="emal"
+                    placeholder="Enter email"
+                    className="form-control"
+                    type="email"
+                    value={ this.state.email }
+                    onChange={ this.onChange }
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <input 
+                    name="name"
+                    id="name"
+                    placeholder="Enter name"
+                    className="form-control"
+                    type="text"
+                    value={ this.state.name }
+                    onChange={ this.onChange }
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="instituteId">InstituteId</label>
+                  <input 
+                    name="instituteId"
+                    id="instituteId"
+                    placeholder="Enter instituteId"
+                    className="form-control"
+                    type="text"
+                    value={ this.state.instituteId }
+                    onChange={ this.onChange }
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">New Password</label>
+                  <input 
+                    name="password"
+                    id="password"
+                    placeholder="Enter password"
+                    className="form-control"
+                    type="password"
+                    value={ this.state.password }
+                    onChange={ this.onChange }
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="rePassword">Re-enter New Password</label>
+                  <input 
+                    name="rePassword"
+                    id="rePassword"
+                    placeholder="Re-enter password"
+                    className="form-control"
+                    type="password"
+                  />
+                </div>
+                <button className="btn btn-success" type="submit">Update Profile</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      ): null
+
+
+    );//
   }
 }
 
