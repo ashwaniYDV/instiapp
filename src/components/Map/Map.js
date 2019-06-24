b344d44e43aa467742d29cd65228fefe9205d6ed
Ashwani Yadav <ashyadavash@gmail.com>
2019-06-25 02:48:16 +0530

    updated ui

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -5,7 +5,7 @@ import MapContainer from './MapContainer';
 class Map extends Component {
     render() {
         return (
-            <div style={{position: 'relative', width: '100%', height: 'calc(100vh - 120px)'}}>
+            <div style={{position: 'relative', width: '100%', height: 'calc(100vh - 65px)'}}>
                 <MapContainer />
             </div>
         )
