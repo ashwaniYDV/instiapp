a7741f7e0676b854c19f7dfb05ff7d67b4718113
Ashwani Yadav <ashyadavash@gmail.com>
2019-07-01 05:58:00 -0700

    updated ui

--- src/components/LoginRequired/LoginRequired.js
+++ src/components/LoginRequired/LoginRequired.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { Typography, Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
-class LoginRequired extends Component {
-    render() {
-        return (
-            <div>
-                Please Login
-            </div>
-        )
-    }
+import { openLoginModal } from "../../redux/actions/authActions";
+
+const styles = theme => ({
+  root: {
+      width: '100%',
+      padding: '25px'
+  },
+  button: {
+      margin: theme.spacing.unit,
+  },
+});
+class LoginRequired extends React.Component {
+
+  openLoginModal = () => {
+    this.props.openLoginModal();
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+    return (
+      <div className={classes.root}>
+        <Typography gutterBottom>
+              Please login to view this page!
+        </Typography>
+        <Button variant="contained" color="secondary" className={classes.button} onClick={this.openLoginModal}>
+          Login
+        </Button>
+      </div>
+    );
+  }
 }
 
-export default LoginRequired;
+export default compose(
+  connect( null, {openLoginModal} ),
+  withStyles(styles, { withTheme: true }),
+)(LoginRequired);
